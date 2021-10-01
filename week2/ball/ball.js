40981ec5b8b370779ba6ac46449e123c03d6f55f
Manuel Patrone <manuel.patrone@students.fhnw.ch>
2021-10-01 17:15:56 +0200

    first version of ball jumping

--- week2/ball/ball.js
+++ week2/ball/ball.js
@@ -1,7 +1,8 @@
 
 const radius = 10;
-const ball = {x:20, y:0, dx: 5, dy: 1};
+const ball = {x:20, y:0, dx: 1, dy: 5};
 let   old  = {x: ball.x, y: ball.y};
+let isFalling = true;
 
 function start() {
     const canvas  = document.getElementById("canvas");
@@ -16,15 +17,24 @@ function start() {
 
 function nextBoard() {
     // keep old ball values for the sake of efficient clearing of the old display
-
+    old.x = ball.x;
+    old.y =ball.y
     // handle ball is hitting the bounds
     //   reverse direction
     //   lose some energy relative to the current inertia (only velocity varies)
-
+    if(ball.y >= 400){
+        ball.dy = ball.dy*-1
+        isFalling=false;
+    }
+    if(ball.y<=10 && ball.dy<=0){
+        ball.dy = ball.dy*-1
+        isFalling=true;
+    }
     // calculate new position
     // calculate any changes in velocity due to gravitational pull or medium resistance
-
-
+    ball.x+=ball.dx
+    ball.y+=ball.dy
+    ball.dy*=isFalling?1.1:0.9
 }
 
 function display(context) {
